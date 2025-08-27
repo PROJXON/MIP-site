259d1bf5a109d62827ad8fcd43eb35127c1ae8e4
davenpos <simmywim@hotmail.com>
2025-08-27 16:23:13 -0400

    Made intern testimonials look nicer

--- frontend/src/components/Testimonials/Testimonials.tsx
+++ frontend/src/components/Testimonials/Testimonials.tsx
@@ -6,26 +6,22 @@ export default function Testimonials() {
   const [testimonials, loading] = useWPFetch<Testimonial>('testimonials');
 
   return (
-    <div>
+    <div className="mb-2">
       {loading ? (
         <LoadingSpinner />
       ) : testimonials.length > 0 ? (
         <ul className="blog-list list-unstyled flex flex-wrap justify-center gap-6 my-5 px-6 max-w-7xl mx-auto">
           {testimonials.map((test) => (
             <li key={test.id}>
-              <div className="d-flex align-items-center mb-4">
-                <div className="me-3 flex-shrink-0">
-                  <img
-                    src={test.image}
-                    alt={test.name}
-                    className="rounded-circle border"
-                    width={65}
-                    height={65}
-                  />
-                </div>
-                <div>
-                  <h4 className="mb-0 fs-5 text-yellow">{test.name}</h4>
-                  {/* <p className="mb-0 small text-gray">{test.title}</p> */}
+              <div className="mb-4 flex">
+                <img
+                  src={test.image}
+                  alt={test.name}
+                  className="rounded-full border w-[65px] h-[65px]"
+                />
+                <div className="flex-1 m-auto">
+                  <h4 className="mb-0 text-[#ffd700] font-bold text-lg">{test.name}</h4>
+                  <p className="mb-0 small text-sm text-gray-400">{test.title}</p>
                 </div>
               </div>
               <p className="gray-opacity text-gray">{test.quote}</p>
