44367db77fb65548c68f76d10df19591a436d2c3
davenpos <simmywim@hotmail.com>
2025-10-01 19:43:40 -0400

    Made testimonials text and images bigger

--- frontend/src/components/Testimonials/Testimonials.tsx
+++ frontend/src/components/Testimonials/Testimonials.tsx
@@ -33,13 +33,11 @@ export default function Testimonials() {
                 <img
                   src={test.image}
                   alt={test.name}
-                  className="rounded-full border w-12 h-12 mb-2"
+                  className="rounded-full border w-24 h-24 mb-2"
                 />
-                <h4 className="mb-0  text-yellow-500 font-bold text-base text-center">
-                  {test.name}
-                </h4>
-                <p className="mb-0 small text-xs text-gray-500 text-center">{test.title}</p>
-                <p className="gray-opacity text-xs text-gray-400 text-center mt-2">{test.quote}</p>
+                <h4 className="mb-0 text-yellow-500 font-bold text-lg text-center">{test.name}</h4>
+                <p className="mb-0 small text-sm text-gray-500 text-center">{test.title}</p>
+                <p className="gray-opacity text-sm text-gray-400 text-center mt-2">{test.quote}</p>
               </div>
             </SwiperSlide>
           ))}
